91c8de124af03c814dfe7cc59cbf1448535dabfd
hafidz erdityo <hafidze15@gmail.com>
2024-12-16 09:23:22 +0700

    feat(new tab icon svg

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -145,7 +145,7 @@ const Projects = ({ setShowNav }) => {
                 <img
                   src={val.image}
                   alt={val.name}
-                  className="grayscale rounded-md w-full h-32 object-cover transition-transform duration-300 hover:scale-105"
+                  className="grayscale rounded-md w-full h-32 object-cover object-top transition-transform duration-300 hover:scale-105"
                 />
               </div>
 
